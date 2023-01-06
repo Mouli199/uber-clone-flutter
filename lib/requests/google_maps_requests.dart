7bbed40a75992a82f79407a66ba3f10b1e202417
Mouli199 <saimouli14@gmail.com>
2023-01-06 20:54:32 +0530

    first commit

---
+++ lib/requests/google_maps_requests.dart
@@ -0,0 +1,18 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+const apiKey = "AIzaSyBhDflq5iJrXIcKpeq0IzLQPQpOboX91lY";
+
+class GoogleMapsServices{
+  var client;
+
+    Future<String> getRouteCoordinates(LatLng l1, LatLng l2)async{
+      String url = "https://maps.googleapis.com/maps/api/directions/json?origin=${l1.latitude},${l1.longitude}&destination=${l2.latitude},${l2.longitude}&key=$apiKey";
+      var response = await client.post(
+          Uri.https);
+      //http.Response response = await http.get(url);
+      Map values = jsonDecode(response.body);
+      // ProjectLog.logIt(TAG, "Predictions", values.toString());
+      return values["routes"][0]["overview_polyline"]["points"];
+    }
+}
